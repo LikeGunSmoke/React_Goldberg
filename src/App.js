c7912392817163285c928ac4fc8c8e1cfbc30e98
Robert Strange <robertjohn23@live.com>
2021-03-30 16:04:19 -0600

    Implement draggable pieces

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import { Canvas, useFrame, useThree } from 'react-three-fiber';
 import { Physics, useSphere, useBox, usePlane } from 'use-cannon';
-import { useDrag } from 'react-use-gesture';
+import { useDrag, useGesture } from 'react-use-gesture';
 
 import '../styles.css';
 
@@ -26,7 +26,7 @@ const Ball = ({args = [0.5, 32, 32]}) => {
   )
 };
 
-const Paddle = ({args = [2, 0.5, 1]}) => {
+const Paddle = ({args, color}) => {
   const {viewport} = useThree();
   const [ref, api] = useBox(() => ({args}))
 
@@ -38,43 +38,46 @@ const Paddle = ({args = [2, 0.5, 1]}) => {
   return (
     <mesh ref={ref}>
       <boxBufferGeometry args={args}/>
-      <meshStandardMaterial color='green'/>
+      <meshStandardMaterial color={color}/>
     </mesh>
   )
 };
 
-const Block = ({ args = [2, 0.5, 1], color, ...props }) => {
+const Block = ({ args, color, ...props }) => {
 
   const {viewport} = useThree();
   const [ref, api] = useBox(() => ({args, ...props}))
 
   return (
   <mesh ref={ref}>
-    <boxBufferGeometry args={[2, 0.5, 1]}/>
+    <boxBufferGeometry args={args}/>
     <meshStandardMaterial color={color}/>
   </mesh>
   )
 };
 
-const Piece = ({ args = [2, 0.5, 1], color, ...props }) => {
+const Piece = ({args, color}) => {
+
+  const box = useRef();
   const [position, setPosition] = useState([0, 0, 0]);
-  // const [ref, api] = useBox(() => ({position: position, ...props}))
-  const ref = useRef();
+  const [ref, api] = useBox(() => ({mass: 0}))
   const { size, viewport } = useThree();
   const aspect = size.width / viewport.width;
-
   const drag = useDrag(({ offset:[x, y] }) => {
     const [,, z] = position;
-    setPosition([x / aspect, -y / aspect, z]);
+    api.position.set(x / aspect, -y / aspect, z);
   }, { pointerEvents: true});
 
-  return <group {...drag()} />
+  return (
+    <>
+    <mesh ref={ref} {...drag()}>
+      <boxBufferGeometry args={args} />
+      <meshStandardMaterial color={color}/>
+    </mesh>
+    </>
+  )
 }
 
-  //  <mesh ref={ref} position={position} {...drag()}>
-  //    <boxBufferGeometry args={[2, 0.5, 1]}/>
-  //    <meshStandardMaterial color={color}/>
-  //  </mesh>
 
 
 export default function App() {
@@ -84,11 +87,13 @@ export default function App() {
       <pointLight position={[10, 10, 5]}/>
       <pointLight position={[-10, -10, -5]}/>
       <Physics gravity={[0, -30, 0]}
-        defaultContactMaterial={{restitution: 1.01}}
+        defaultContactMaterial={{restitution: 1.1}}
       >
         <Ball />
-        <Paddle />
-        <Block color='orange' position={[1, 1, 0]} />
+        <Paddle args={[2, 0.5, 1]} color='green'/>
+        <Block color='orange' args={[2, 0.5, 1]} position={[5, 5, 0]} />
+        <Piece args={[4, 1, 1]} color='blue'/>
+        <Piece args={[1, 1, 1]} color='red'/>
       </Physics>
   </Canvas>
   )
